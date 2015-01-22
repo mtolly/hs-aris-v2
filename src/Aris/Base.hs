2cf0a1d04bf6fcdaf1cef5e014d7b0a07e233c85
Michael Tolly <miketolly@gmail.com>
2015-01-22 00:16:15 -0600

    more automated record types

--- src/Aris/Base.hs
+++ src/Aris/Base.hs
@@ -10,7 +10,6 @@ import Network.HTTP (simpleHTTP, postRequestWithBody, getResponseBody)
 import qualified Data.Aeson as A
 import qualified Data.Aeson.TH as ATH
 import Data.Aeson ((.:))
-import Data.Aeson.Types (Parser)
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as TE
 import qualified Data.ByteString.Lazy as BL
@@ -58,58 +57,53 @@ getGame i = callAris "games.getGame" $ A.object
   [ ("game_id", A.Number $ fromIntegral i)
   ]
 
-data Game = Game
-  { gameID :: Int             -- $game->game_id                                      = $sql_game->game_id;
-  , gameName :: String        -- $game->name                                         = $sql_game->name;
-  , gameDescription :: String -- $game->description                                  = $sql_game->description;
-  , iconMediaID :: Int        -- $game->icon_media_id                                = $sql_game->icon_media_id;
-  , mediaID :: Int            -- $game->media_id                                     = $sql_game->media_id;
-  , published :: Bool         -- $game->published                                    = $sql_game->published;
-  , gameType :: GameType      -- $game->type                                         = $sql_game->type;
-  , introSceneID :: Int       -- $game->intro_scene_id                               = $sql_game->intro_scene_id;
-  , mapType :: MapType        -- $game->map_type                                     = $sql_game->map_type;
-  , mapLatitude :: Double     -- $game->map_latitude                                 = $sql_game->map_latitude;
-  , mapLongitude :: Double    -- $game->map_longitude                                = $sql_game->map_longitude;
-  , mapZoomLevel :: Double    -- $game->map_zoom_level                               = $sql_game->map_zoom_level;
-  , mapShowPlayer :: Bool     -- $game->map_show_player                              = $sql_game->map_show_player;
-  , mapShowPlayers :: Bool    -- $game->map_show_players                             = $sql_game->map_show_players;
-  , mapOffsiteMode :: Bool    -- $game->map_offsite_mode                             = $sql_game->map_offsite_mode;
-  , notebookAllowComments :: Bool -- $game->notebook_allow_comments                      = $sql_game->notebook_allow_comments;
-  , notebookAllowLikes :: Bool -- $game->notebook_allow_likes                         = $sql_game->notebook_allow_likes;
-  -- $game->notebook_trigger_scene_id                    = $sql_game->notebook_trigger_scene_id;
-  -- $game->notebook_trigger_requirement_root_package_id = $sql_game->notebook_trigger_requirement_root_package_id;
-  -- $game->notebook_trigger_title                       = $sql_game->notebook_trigger_title;
-  -- $game->notebook_trigger_icon_media_id               = $sql_game->notebook_trigger_icon_media_id;
-  -- $game->notebook_trigger_distance                    = $sql_game->notebook_trigger_distance;
-  -- $game->notebook_trigger_infinite_distance           = $sql_game->notebook_trigger_infinite_distance;
-  -- $game->notebook_trigger_wiggle                      = $sql_game->notebook_trigger_wiggle;
-  -- $game->notebook_trigger_show_title                  = $sql_game->notebook_trigger_show_title;
-  -- $game->notebook_trigger_hidden                      = $sql_game->notebook_trigger_hidden;
-  -- $game->notebook_trigger_on_enter                    = $sql_game->notebook_trigger_on_enter;
-  , inventoryWeightCap :: Int -- $game->inventory_weight_cap                         = $sql_game->inventory_weight_cap;
-  } deriving (Eq, Ord, Show, Read)
+newtype AsStr a = AsStr { runAsStr :: a }
+  deriving (Eq, Ord, Show, Read, Functor, Foldable, Traversable)
 
-instance A.FromJSON Game where
-  parseJSON = A.withObject "game object" $ \obj -> do
-    gameID          <- parseRead $ obj .: "game_id"
-    gameName        <-             obj .: "name"
-    gameDescription <-             obj .: "description"
-    iconMediaID     <- parseRead $ obj .: "icon_media_id"
-    mediaID         <- parseRead $ obj .: "media_id"
-    published       <- parseBool $ obj .: "published"
-    gameType        <-             obj .: "type"
-    introSceneID    <- parseRead $ obj .: "intro_scene_id"
-    mapType         <-             obj .: "map_type"
-    mapLatitude     <- parseRead $ obj .: "map_latitude"
-    mapLongitude    <- parseRead $ obj .: "map_longitude"
-    mapZoomLevel    <- parseRead $ obj .: "map_zoom_level"
-    mapShowPlayer   <- parseBool $ obj .: "map_show_player"
-    mapShowPlayers  <- parseBool $ obj .: "map_show_players"
-    mapOffsiteMode  <- parseBool $ obj .: "map_offsite_mode"
-    notebookAllowComments <- parseBool $ obj .: "notebook_allow_comments"
-    notebookAllowLikes    <- parseBool $ obj .: "notebook_allow_likes"
-    inventoryWeightCap    <- parseRead $ obj .: "inventory_weight_cap"
-    return Game{..}
+instance (Read a) => A.FromJSON (AsStr a) where
+  parseJSON = A.withText "value stored as string" $ \txt ->
+    case readMaybe $ T.unpack txt of
+      Nothing -> fail $ "couldn't read value from String: " ++ show txt
+      Just x  -> return $ AsStr x
+
+newtype StrBool = StrBool { runStrBool :: Bool }
+  deriving (Eq, Ord, Show, Read)
+
+instance A.FromJSON StrBool where
+  parseJSON (A.String "0") = return $ StrBool False
+  parseJSON (A.String "1") = return $ StrBool True
+  parseJSON _ = fail "expected bool as \"0\" \"1\""
+
+data Game = Game
+  { g_game_id :: AsStr Int
+  , g_name :: String
+  , g_description :: String
+  , g_icon_media_id :: AsStr Int
+  , g_media_id :: AsStr Int
+  , g_map_type :: MapType
+  , g_map_latitude :: AsStr Double
+  , g_map_longitude :: AsStr Double
+  , g_map_zoom_level :: AsStr Double
+  , g_map_show_player :: StrBool
+  , g_map_show_players :: StrBool
+  , g_map_offsite_mode :: StrBool
+  , g_notebook_allow_comments :: StrBool
+  , g_notebook_allow_likes :: StrBool
+  , g_notebook_trigger_scene_id :: AsStr Int
+  , g_notebook_trigger_requirement_root_package_id :: AsStr Int
+  , g_notebook_trigger_title :: String
+  , g_notebook_trigger_icon_media_id :: AsStr Int
+  , g_notebook_trigger_distance :: AsStr Int
+  , g_notebook_trigger_infinite_distance :: StrBool
+  , g_notebook_trigger_wiggle :: StrBool
+  , g_notebook_trigger_show_title :: StrBool
+  , g_notebook_trigger_hidden :: StrBool
+  , g_notebook_trigger_on_enter :: StrBool
+  , g_inventory_weight_cap :: AsStr Int
+  , g_published :: StrBool
+  , g_type :: GameType
+  , g_intro_scene_id :: AsStr Int
+  } deriving (Eq, Ord, Show, Read)
 
 data GameType
   = Location
@@ -123,20 +117,6 @@ data MapType
   | Hybrid
   deriving (Eq, Ord, Show, Read, Enum, Bounded)
 
-parseBool :: Parser String -> Parser Bool
-parseBool p = do
-  s <- p
-  case s of
-    "0" -> return False
-    "1" -> return True
-    _   -> fail $ "couldn't read Bool from String: " ++ show s
-
-parseRead :: (Read a) => Parser String -> Parser a
-parseRead p = do
-  s <- p
-  case readMaybe s of
-    Just x -> return x
-    Nothing -> fail $ "couldn't read value from String: " ++ show s
-
+ATH.deriveFromJSON ATH.defaultOptions{ ATH.fieldLabelModifier = drop 2 } ''Game
 ATH.deriveJSON ATH.defaultOptions{ ATH.constructorTagModifier = map toUpper } ''GameType
 ATH.deriveJSON ATH.defaultOptions{ ATH.constructorTagModifier = map toUpper } ''MapType
