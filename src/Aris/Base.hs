c51ebb8f8d7ac5cfdf26e52ebb5fe3f84a2bf589
Michael Tolly <miketolly@gmail.com>
2015-01-21 19:37:51 -0600

    parse return-code-wrapped json objects

--- src/Aris/Base.hs
+++ src/Aris/Base.hs
@@ -1,20 +1,40 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Aris.Base where
 
 import Network.HTTP (simpleHTTP, postRequestWithBody, getResponseBody)
 import qualified Data.Aeson as A
+import Data.Aeson ((.:))
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as TE
 import qualified Data.ByteString.Lazy as BL
-import Data.Maybe (fromJust)
 
-callAris :: String -> A.Value -> IO A.Value
+callAris :: String -> A.Value -> IO (Return A.Value)
 callAris fun val = do
   rsp <- simpleHTTP $ postRequestWithBody
     ("http://dev.arisgames.org/server/json.php/v2." ++ fun)
     "application/x-www-form-urlencoded"
     (T.unpack $ TE.decodeUtf8 $ BL.toStrict $ A.encode val)
-  fmap (fromJust . A.decodeStrict' . TE.encodeUtf8 . T.pack) $ getResponseBody rsp
+  body <- getResponseBody rsp
+  case A.eitherDecodeStrict $ TE.encodeUtf8 $ T.pack body of
+    Right x  -> return x
+    Left err -> error $ "Aris.Base.callAris: invalid JSON returned by API; "
+      ++ err ++ "; response body: " ++ body
 
-getGame :: Int -> IO A.Value
+data Return a
+  -- = Fault { faultCode :: String, faultDetail :: String, faultString :: String }
+  = Error Int String
+  | Data a
+  deriving (Eq, Ord, Show, Read)
+
+instance (A.FromJSON a) => A.FromJSON (Return a) where
+  parseJSON = A.withObject "return-code-wrapped object" $ \obj -> do
+    code <- obj .: "returnCode"
+    if code == 0
+      then fmap Data $ obj .: "data"
+      else do
+        desc <- obj .: "returnCodeDescription"
+        return $ Error code desc
+
+getGame :: Int -> IO (Return A.Value)
 getGame i = callAris "games.getGame" $
   A.object [(T.pack "game_id", A.Number $ fromIntegral i)]
